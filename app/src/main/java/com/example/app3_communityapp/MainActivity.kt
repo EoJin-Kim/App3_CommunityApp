d69f9a37b44e437d7f4a06ea92bbd0aada6cf826
EoJin-Kim <linexy@naver.com>
2022-06-29 14:37:04 +0900

    Feat : Init

---
+++ app/src/main/java/com/example/app3_communityapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.app3_communityapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.app3_communityapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var mainActivityBinding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mainActivityBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(mainActivityBinding.root)
+    }
+}
