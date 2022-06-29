631f14b3811c48771564518aa6345e43748f8685
EoJin-Kim <linexy@naver.com>
2022-06-29 16:07:30 +0900

    Feat : 로그인 화면 구성

--- app/src/main/java/com/example/app3_communityapp/MainActivity.kt
+++ app/src/main/java/com/example/app3_communityapp/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.app3_communityapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.example.app3_communityapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var mainActivityBinding : ActivityMainBinding
+    lateinit var currentFragment : Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +21,31 @@ class MainActivity : AppCompatActivity() {
         mainActivityBinding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(mainActivityBinding.root)
+
+        fragmentController("login",false,false)
+    }
+
+    fun fragmentController(name: String, add: Boolean, animate: Boolean) {
+        when(name){
+            "login" -> {
+                currentFragment = LoginFragment()
+            }
+
+
+        }
+
+        val trans = supportFragmentManager.beginTransaction()
+        trans.replace(R.id.main_container, currentFragment)
+
+        if (add) {
+            trans.addToBackStack(name)
+        }
+
+        if (animate) {
+            trans.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+        }
+
+        trans.commit()
+
     }
 }
