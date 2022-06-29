c21817b6805565abe24d6a623583358cc446fade
EoJin-Kim <linexy@naver.com>
2022-06-29 15:24:17 +0900

    Feat : Splash 화면 구성

--- app/src/main/java/com/example/app3_communityapp/MainActivity.kt
+++ app/src/main/java/com/example/app3_communityapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.app3_communityapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import com.example.app3_communityapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        SystemClock.sleep(1000)
+        setTheme(R.style.Theme_App3_CommunityApp)
 
         mainActivityBinding = ActivityMainBinding.inflate(layoutInflater)
 
