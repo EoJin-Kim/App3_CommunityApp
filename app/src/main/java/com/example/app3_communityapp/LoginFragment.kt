327a428047e57121dc6708bf2c0ad8f163017582
김어진 <linexy@naver.com>
2022-06-30 23:01:34 +0900

    Feat : 로그인 처리

--- app/src/main/java/com/example/app3_communityapp/LoginFragment.kt
+++ app/src/main/java/com/example/app3_communityapp/LoginFragment.kt
@@ -1,5 +1,6 @@
 package com.example.app3_communityapp
 
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
@@ -10,6 +11,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import com.example.app3_communityapp.databinding.FragmentLoginBinding
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import kotlin.concurrent.thread
 
 
 class LoginFragment : Fragment() {
@@ -70,9 +75,75 @@ class LoginFragment : Fragment() {
                 return@setOnClickListener
             }
 
-            Log.d("test","$loginId")
-            Log.d("test","$loginPw")
-            Log.d("test","$loginAutoLogin")
+//            Log.d("test","$loginId")
+//            Log.d("test","$loginPw")
+//            Log.d("test","$loginAutoLogin")
+
+            thread {
+                val client =OkHttpClient()
+
+                val site = "http://219.248.58.57:8080/login_user"
+
+                val builder1 = FormBody.Builder()
+                builder1.add("user_id",loginId)
+                builder1.add("user_pw",loginPw)
+                val formBody = builder1.build()
+
+                val request = Request.Builder().url(site).post(formBody).build()
+                val response = client.newCall(request).execute()
+
+                if(response.isSuccessful){
+                    // 응답 결과를 추출한다.
+                    val result_test = response.body?.string()!!.trim()
+//                    Log.d("test",result_test)
+
+                    if(result_test == "FAIL"){
+                        activity?.runOnUiThread {
+                            val dialogBuilder = AlertDialog.Builder(requireContext())
+                            dialogBuilder.setTitle("로그인 실패")
+                            dialogBuilder.setMessage("아이디나 비밀번호가 잘못되었습니다")
+                            dialogBuilder.setPositiveButton("확인"){ dialogInterface: DialogInterface, i: Int ->
+                                loginFragmentBinding.loginId.setText("")
+                                loginFragmentBinding.loginPw.setText("")
+                                loginFragmentBinding.loginAutologin.isChecked= false
+                                loginFragmentBinding.loginAutologin.requestFocus()
+                            }
+                            dialogBuilder.show()
+                        }
+                    }
+                    else{
+                        activity?.runOnUiThread {
+                            val dialogBuilder = AlertDialog.Builder(requireContext())
+                            dialogBuilder.setTitle("로그인 성공")
+                            dialogBuilder.setMessage("로그인에 성공하였습니다")
+                            dialogBuilder.setPositiveButton("확인"){ dialogInterface: DialogInterface, i: Int ->
+
+                                // 사용자 정보를 preferences에 저장한다
+                                val pref = activity?.getSharedPreferences("login_data",Context.MODE_PRIVATE)
+                                val editor = pref?.edit()
+
+                                editor?.putInt("login_user_idx",Integer.parseInt(result_test))
+                                editor?.putInt("login_auto_login",loginAutoLogin)
+
+                                val boardMainIntent = Intent(requireContext(),BoardMainActivity::class.java)
+                                startActivity(boardMainIntent)
+                                activity?.finish()
+
+                            }
+                            dialogBuilder.show()
+                        }
+                    }
+                }else{
+                    activity?.runOnUiThread {
+                        val dialogBuilder = AlertDialog.Builder(requireContext())
+                        dialogBuilder.setTitle("로그인 오류")
+                        dialogBuilder.setMessage("로그인 오류가 발생하였습니다")
+                        dialogBuilder.setPositiveButton("확인",null)
+                        dialogBuilder.show()
+
+                    }
+                }
+            }
 
 //            val boardMainIntent = Intent(requireContext(),BoardMainActivity::class.java)
 //            startActivity(boardMainIntent)
